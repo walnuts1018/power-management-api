3a15d36adab6648fad5d254f57e73a4a24dd0d72
walnuts1018 <r.juglans.1018@gmail.com>
2023-05-07 23:30:15 +0900

    add main

--- src/main.go
+++ src/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"os"
+)
+
+var token = os.Getenv("SWITCHBOT_TOKEN")
+var secret = os.Getenv("SWITCHBOT_SECRET")
+var url = "https://api.switch-bot.com"
+
+func main() {
+
+}
